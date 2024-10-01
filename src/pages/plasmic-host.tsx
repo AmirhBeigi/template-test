0c985cdab490bef2c17f6d53be13e911aa9fc6ad
AmirhBeigi <github-action@actions-template-sync.noreply.github.com>
2024-10-01 01:05:35 +0000

    chore(template): merge template changes :up:
    Signed-off-by: AmirhBeigi <github-action@actions-template-sync.noreply.github.com>

--- src/pages/plasmic-host.tsx
+++ src/pages/plasmic-host.tsx
@@ -11,6 +11,13 @@ import { DatePicker, datePickerMeta } from "@/fragment/components/date-picker";
 import { TimePicker, timePickerMeta } from "@/fragment/components/time-picker";
 import { Splunk, splunkMeta } from "@/fragment/splunk";
 import { Popover, popoverMeta } from "@/fragment/components/popover";
+import { Select, selectMeta } from "@/fragment/components/select";
+import { Input, inputMeta } from "@/fragment/components/input";
+import { Switch, switchMeta } from "@/fragment/components/switch";
+import { ApiRequest, apiRequestMeta } from "@/fragment/components/api-request";
+import { Slider, sliderMeta } from "@/fragment/components/slider";
+import { Chart, chartMeta } from "@/fragment/components/chart";
+import { Textarea, textareaMeta } from "@/fragment/components/textarea";
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
@@ -22,3 +29,10 @@ registerGlobalContext(Splunk, splunkMeta);
 registerComponent(DatePicker, datePickerMeta);
 registerComponent(TimePicker, timePickerMeta);
 registerComponent(Popover, popoverMeta);
+registerComponent(Select, selectMeta);
+registerComponent(Input, inputMeta);
+registerComponent(Switch, switchMeta);
+registerComponent(Slider, sliderMeta);
+registerComponent(ApiRequest, apiRequestMeta);
+registerComponent(Chart, chartMeta);
+registerComponent(Textarea, textareaMeta);
